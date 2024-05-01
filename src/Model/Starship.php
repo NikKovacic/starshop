cc2053bb7a89cf9809de60984399d2768079f9b8
NikKovacic <nik.kovacic10@gmail.com>
2024-05-01 18:03:42 +0200

    Creating your own service

--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -13,45 +13,28 @@ class Starship
     ) {
     }
 
-    /**
-     * @return int
-     */
     public function getId(): int
     {
         return $this->id;
     }
 
-    /**
-     * @return string
-     */
     public function getName(): string
     {
         return $this->name;
     }
 
-    /**
-     * @return string
-     */
     public function getClass(): string
     {
         return $this->class;
     }
 
-    /**
-     * @return string
-     */
     public function getCaptain(): string
     {
         return $this->captain;
     }
 
-    /**
-     * @return string
-     */
     public function getStatus(): string
     {
         return $this->status;
     }
-
-
 }
