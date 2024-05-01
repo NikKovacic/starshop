ca7125391c3922e49f8151af9644457899f6ea9b
NikKovacic <nik.kovacic10@gmail.com>
2024-05-01 18:24:42 +0200

    Fancier Routes: Requirements, Wildcards, and More

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -7,13 +7,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/api/starships')]
 class StarshipApiController extends AbstractController
 {
-    #[Route('/api/starships')]
+    #[Route('/', methods: ['GET'])]
     public function getCollection(StarshipRepository $repository): Response
     {
         $starships = $repository->findAll();
 
         return $this->json($starships);
+    }
+
+    #[Route('/{id<\d+>}', methods: ['GET'])]
+    public function get(int $id, StarshipRepository $repository): Response
+    {
+        // note to myself: </d+> in route annotation is regex for int!
+        $starship = $repository->find($id);
+        if (!$starship) {
+            throw $this->createNotFoundException('Starship not found');
+        }
+
+        return $this->json($starship);
     }
 }
